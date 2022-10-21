b2aa333a14f6eac5a4f0ce39e7f98b81148986dc
Pawan <Pawan@192.168.0.104>
2022-10-21 09:28:17 +0530

    pushing the code first time

---
+++ project_6/src/project_6/pawan.java
@@ -0,0 +1,35 @@
+package project_6;
+
+public class pawan 
+{
+ public void method()
+ {
+	 this.method1();
+	 System.out.println("default method");
+ }
+ public void method1()
+ {
+	 this.method2();
+	 System.out.println("one parameterized method");
+ }
+ public void method2()
+ {
+	 this.method3();
+	 System.out.println("two parameterized method");
+ }
+ public void method3()
+ {
+	 this.method4();
+	 System.out.println("three parameterized method");
+ }
+ public void method4()
+ {
+	 System.out.println("four parameterized method");
+ }
+ public static void main(String[] args) 
+ {
+	pawan obj=new pawan();
+	obj.method();
+}
+ 
+}
